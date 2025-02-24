ea2eb3a371453fca6b7b636b3f676db18f9e0cc2
Rakesh352004 <ry352004@gmail.com>
2025-02-24 15:27:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import numpy as np
 import gdown
 import os
 
-file_id = "1_9Q28QeJXnyRCH18IVxpX5KhuDtZC40c"
-url = 'https://drive.google.com/uc?id=1_9Q28QeJXnyRCH18IVxpX5KhuDtZC40c'
+file_id = "1em_4pDcMztrkxm1gH0WWmf9I8brKAzkj"
+url = 'https://drive.google.com/file/d/1em_4pDcMztrkxm1gH0WWmf9I8brKAzkj/view?usp=sharing'
 model_path = "trained_plant_disease_model.keras"
 
 
